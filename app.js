229de455c1e6f6859e35787f59d5de93aab42486
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-08-14 17:16:43 -0700

    chained route handlers
    - added chained route handlers for "/articles" route

--- app.js
+++ app.js
@@ -21,6 +21,34 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.route("/articles")
+.get(function(req, res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        }else{
+            res.send(err);
+        }
+    });
+})
+.post(function(req, res){
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+})
+.delete(function(req, res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("Successfully deleted all articles.");
+        }else{
+            res.send(err);
+        }
+    });
+});
+
+
+/*
 //GET route
 app.get("/articles", function(req, res){
     //looks articles in db
@@ -62,6 +90,7 @@ app.delete("/articles", function(req, res){
         }
     });
 });
+*/
 
 app.listen(3000, function(){
     console.log("Server started on port 3000.");
